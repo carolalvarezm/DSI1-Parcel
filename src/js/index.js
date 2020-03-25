2bdc6c366566643a0dda37abc92913316bd52b1b
Carolina Álvarez Martín <alu0100944723@ull.edu.es>
2020-03-25 11:45:33 +0000

    Pruebas con prettier y eslint

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,13 @@
-const item1 = document.querySelector(".item1");
-const item2 = document.querySelector(".item2");
-const item3 = document.querySelector(".item3");
+const item1 = document.querySelector('.item1');
+const item2 = document.querySelector('.item2');
+const item3 = document.querySelector('.item3');
 
 item1.onclick = () => {
-  item1.classList.toggle("box2");
-}
+    item1.classList.toggle('box2');
+};
 item2.onclick = () => {
-    item2.classList.toggle("box2");
-}
+    item2.classList.toggle('box2');
+};
 item3.onclick = () => {
-    item3.classList.toggle("box2");
-}
+    item3.classList.toggle('box2');
+};
